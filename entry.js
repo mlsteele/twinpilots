e95a03e9ddea54afddd34504308ef0b3074a30fc
Miles Steele <miles@milessteele.com>
2016-03-12 21:14:18 -0500

    spaces (argh)

--- entry.js
+++ entry.js
@@ -11,35 +11,34 @@ loadShipModel(_ => {
     var gameport = new GamePort()
     gameport.animate();
 
-
     var gamestate = new GameState()
 
     ;(function gameloop() {
-	// Schedule next iteration.
-	requestAnimationFrame(gameloop)
+        // Schedule next iteration.
+        requestAnimationFrame(gameloop)
 
-	// Use keyboard state to modify game state.
-	gamestate.ships[0].thrusters.forward = bool_to_bin(kbinput.isDown("KeyW"))
-	// gamestate.ships[0].thrusters.reverse = bool_to_bin(kbinput.isDown("KeyS"))
-	gamestate.ships[0].thrusters.ccw = bool_to_bin(kbinput.isDown("KeyA"))
-	gamestate.ships[0].thrusters.cw  = bool_to_bin(kbinput.isDown("KeyD"))
+        // Use keyboard state to modify game state.
+        gamestate.ships[0].thrusters.forward = bool_to_bin(kbinput.isDown("KeyW"))
+        // gamestate.ships[0].thrusters.reverse = bool_to_bin(kbinput.isDown("KeyS"))
+        gamestate.ships[0].thrusters.ccw = bool_to_bin(kbinput.isDown("KeyA"))
+        gamestate.ships[0].thrusters.cw  = bool_to_bin(kbinput.isDown("KeyD"))
 
-	gamestate.ships[1].thrusters.forward = bool_to_bin(kbinput.isDown("KeyI"))
-	// gamestate.ships[1].thrusters.reverse = bool_to_bin(kbinput.isDown("KeyK"))
-	gamestate.ships[1].thrusters.ccw = bool_to_bin(kbinput.isDown("KeyJ"))
-	gamestate.ships[1].thrusters.cw  = bool_to_bin(kbinput.isDown("KeyL"))
+        gamestate.ships[1].thrusters.forward = bool_to_bin(kbinput.isDown("KeyI"))
+        // gamestate.ships[1].thrusters.reverse = bool_to_bin(kbinput.isDown("KeyK"))
+        gamestate.ships[1].thrusters.ccw = bool_to_bin(kbinput.isDown("KeyJ"))
+        gamestate.ships[1].thrusters.cw  = bool_to_bin(kbinput.isDown("KeyL"))
 
-	gamestate.stepPhysics()
+        gamestate.stepPhysics()
 
-	// Update renderer about game.
-	gameport.update(gamestate)
+        // Update renderer about game.
+        gameport.update(gamestate)
     })()
 
     function bool_to_bin(v) {
-	if (v) {
-	    return 1
-	} else {
-	    return 0
-	}
+        if (v) {
+            return 1
+        } else {
+            return 0
+        }
     }
 })
--- gamestate.js
+++ gamestate.js
@@ -1,58 +1,58 @@
 class GameState {
     constructor() {
-	this.ships = []
-	this.addShip()
-	this.addShip()
-	this.ships[1].pos.x = 50
+        this.ships = []
+        this.addShip()
+        this.addShip()
+        this.ships[1].pos.x = 50
     }
 
     addShip() {
-	var ship = {
-	    pos: {
-		x: 0,
-		y: 0,
-		// Yaw in radians.
-		heading: Math.PI/2,
-	    },
-	    vel: {
-		x: 0,
-		y: 0,
-		rotation: 0,
-	    },
-	    // Thrust is 0 or 1.
-	    thrusters: {
-		forward: 0, // 0 or 1.
-		cw: 0, // 0 or 1.
-		ccw: 0, // 0 or 1.
-	    }
-	}
-	this.ships.push(ship)
+        var ship = {
+            pos: {
+                x: 0,
+                y: 0,
+                // Yaw in radians.
+                heading: Math.PI/2,
+            },
+            vel: {
+                x: 0,
+                y: 0,
+                rotation: 0,
+            },
+            // Thrust is 0 or 1.
+            thrusters: {
+                forward: 0, // 0 or 1.
+                cw: 0, // 0 or 1.
+                ccw: 0, // 0 or 1.
+            }
+        }
+        this.ships.push(ship)
     }
 
     stepPhysics() {
-	var timestep = 1 / 100
+        var timestep = 1 / 100
 
-	for (let ship of this.ships) {
-	    // Directional thrust.
-	    var thrust_factor = 180
-	    var thrust_x = Math.cos(ship.pos.heading) * ship.thrusters.forward * thrust_factor
-	    var thrust_y = Math.sin(ship.pos.heading) * ship.thrusters.forward * thrust_factor
-	    ship.vel.x += thrust_x * timestep
-	    ship.vel.y += thrust_y * timestep
+        for (let ship of this.ships) {
+            // Directional thrust.
+            var thrust_factor = 180
+            var thrust_x = Math.cos(ship.pos.heading) * ship.thrusters.forward * thrust_factor
+            var thrust_y = Math.sin(ship.pos.heading) * ship.thrusters.forward * thrust_factor
+            ship.vel.x += thrust_x * timestep
+            ship.vel.y += thrust_y * timestep
 
-	    // Rotational thrust.
-	    var rotation_thrust_factor = 0.1
-	    ship.vel.rotation += ship.thrusters.ccw * rotation_thrust_factor * timestep
-	    ship.vel.rotation -= ship.thrusters.cw  * rotation_thrust_factor * timestep
+            // Rotational thrust.
+            var rotation_thrust_factor = 0.1
+            ship.vel.rotation += ship.thrusters.ccw * rotation_thrust_factor * timestep
+            ship.vel.rotation -= ship.thrusters.cw  * rotation_thrust_factor * timestep
 
-	    // Integrate.
-	    ship.pos.x += ship.vel.x * timestep
-	    ship.pos.y += ship.vel.y * timestep
-	    ship.pos.heading += ship.vel.rotation
+            // Integrate.
+            ship.pos.x += ship.vel.x * timestep
+            ship.pos.y += ship.vel.y * timestep
+            ship.pos.heading += ship.vel.rotation
 
-	    // Dampening
-	    ship.vel.rotation *= Math.pow(.2, timestep)
-	}
+            // Dampening
+            ship.vel.rotation *= Math.pow(.2, timestep)
+        }
     }
 }
 
